c939142a3db03d7ec73b222f1cdbb0cd6ecabcdc
jonassed <eagle_fly357@hotmail.com>
2015-04-10 13:32:45 +0200

    matlab

---
+++ MatlabBackup/ProjectMatlab/LMSattempt1.m
@@ -0,0 +1,96 @@
+clc; clear; close all;
+%addpath('mfiles');
+
+% load data
+[rec1,fs]=audioread('VOICENOISE1.m4a');
+rec1=rec1(900000:1250000);
+[rec2,fs]=audioread('NOISE1.m4a');
+rec2=rec2(900000:1250000);
+%rec1=rec1(900000:904096);
+%rec2=rec2(900000:904096);
+% model
+% rec1= z(n)=s(n)+x(n), s(n)=signal, x(n)=noise
+% rec2= y(n)          , y(n)=noise
+% x(n), y(n) correlated
+% estimate via noise subtraction
+% s^(n)=z(n)-x^(n)
+z=rec1;
+y=rec2;
+
+
+%%
+muu=3e-3;
+N=1000; % filter order
+M=length(z);
+xhat=zeros(length(z),1);
+sparseC = 1;
+
+thetahat=zeros(1,N/sparseC);
+
+
+
+% Loop
+tic
+for n=1:M,
+
+	% Generate Y. Set elements of Y that does not exist to zero
+    Y=zeros(N/sparseC,1);
+    Y(1:min(N/sparseC,n/sparseC),1)=flip(rec2(max(1,n-N+1):sparseC:n-sparseC+1));
+    
+	% Estimate of x
+    xhat(n,1)=thetahat*Y;
+
+	% Update the n+1 row in the matrix thetahat which in the notation in the Lecture Notes
+	% corresponds to thetahat(n)
+
+	thetahat=thetahat+(muu*Y*(rec1(n)-xhat(n,1))).';
+    
+end
+toc
+
+%%
+muu=3e-3;
+N=1000; % filter order
+M=length(z);
+xhat2=zeros(length(z),1);
+thetahat2=zeros(1,N);
+
+% Loop
+
+
+
+tic
+for n=1:M,
+
+	% Generate Y. Set elements of Y that does not exist to zero
+    Y=zeros(N,1);
+    Y(1:min(N,n),1)=flip(rec2(max(1,n-N+1):n));
+    
+    
+	% Estimate of x
+    xhat2(n,1)=thetahat2*Y;
+    
+    
+	% Update the n+1 row in the matrix thetahat which in the notation in the Lecture Notes
+	% corresponds to thetahat(n)
+
+	thetahat2=thetahat2+(muu*Y*(rec1(n)-xhat2(n,1))).';
+end
+toc
+%%
+shatLMS=z-xhat;
+soundsc(z-xhat,fs);
+pause(8)
+%soundsc(z-xhat2,fs);
+PLOT=1;
+if PLOT
+figure
+pwelch(rec1,3000);
+figure
+pwelch(xhatLMS,3000);
+end
+
+%%
+
+%rec1downsample = downsample(rec1,2);
+%rec2downsample = downsample(rec1(2:),2);
